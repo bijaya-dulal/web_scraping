6ebd4c829c42f348f959b64ed9de460a5a87a402
bijaya-dulal <dulalbijaya0@gmail.com>
2024-03-06 23:33:47 +0545

    css updated

--- scrap.py
+++ scrap.py
@@ -35,7 +35,7 @@ def scrape_cricket_scores():
         # Generate HTML table
         html_table = '<!DOCTYPE html>\n<html>\n<head>\n<title>Cricket Scores</title>\n<link rel="stylesheet" type="text/css" href="design.css">\n</head>\n<body>\n'
         html_table += '<h1>Live Cricket Scores</h1>\n<table border="1">'
-        html_table += f'<h4>Updated on: {pub_date}</h4>\n<table border="1">'
+        html_table += f'<h3>Updated on: {pub_date}</h3>\n<table border="1">'
         html_table += '<tr><th>Country 1</th><th> VS </th><th>Country 2</th></tr>'
         for i in range(len(countries)):
             html_table += f'<tr><td>{countries[i]}</td><td> VS </td><td>{scores[i]}</td></tr>'
