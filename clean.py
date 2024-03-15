4317b989eef54e566c529e72ba856856920e18ab
bijaya-dulal <dulalbijaya0@gmail.com>
2024-03-15 17:59:39 +0545

    score & country seperated

--- clean.py
+++ clean.py
@@ -1,4 +1,23 @@
 import re
 
-def clean_data ():
-    pass
+# Regular expression pattern to match the country name and score
+pattern = r'(.+)\s(\d+/\d+)'
+
+def clean_data(data_list):
+    print("i have called")
+    for data in data_list:
+        match = re.match(pattern, data)
+        if match:
+            country = match.group(1)
+            score = match.group(2)
+            print("Country:", country)
+            print("Score:", score)
+        else:
+            print(data,"yet to begin")
+        print()  # Add a newline between each data entry
+
+# Example list of data entries
+data_list = ["New Zealand 179/10", "Australia 383/10", "India 240/7"]
+
+# Call the clean_data function with the list of data
+#clean_data(data_list)
--- scrap.py
+++ scrap.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+from clean import clean_data
 
 def scrape_cricket_scores():
+    print("here")
     # URL of the Google search page for live cricket scores
     url = 'https://static.cricinfo.com/rss/livescores.xml'
     # Send a GET request to fetch the HTML content of the page
     response = requests.get(url)
    
-
     if response.status_code == 200:
         # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,21 +17,21 @@ def scrape_cricket_scores():
         score_elements = soup.find_all('title',)
         pub_date_tag = soup.find('pubdate')
         pub_date = pub_date_tag.text.strip()
-        print(pub_date_tag)
         # Extract country and score from each match
         countries1= []
         countries2 = []
+       
         for score_element in score_elements:
             score_text = score_element.text.strip()
             
-            # Use regular expression to extract country and score
+            # Use regular expression to seperate the country
             match = re.match(r'(.*) v (.*)', score_text)
             if match:
                 country = match.group(1).strip()
                 score = match.group(2).strip()
                 countries1.append(country)
                 countries2.append(score)
-
+        clean_data(countries1)
         # Generate HTML table
         html_table = '<!DOCTYPE html>\n<html>\n<head>\n<title>Cricket Scores</title>\n<link rel="stylesheet" type="text/css" href="design.css">\n</head>\n<body>\n'
         html_table += '<h1>Live Cricket Scores</h1>\n<table border="1">'
@@ -45,8 +45,9 @@ def scrape_cricket_scores():
         with open('cricket_scores.html', 'w') as f:
             f.write(html_table)
 
-        print("HTML table generated successfully.")
+        
     else:
         print('Failed to fetch the live cricket scores. Status code:', response.status_code)
 
 
+scrape_cricket_scores()
